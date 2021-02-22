f790251291bef263dfdbb9712d13e56a4ab3fa4f
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2021-02-22 20:42:21 +0200

    added constant

--- src/main/java/MyRunnable.java
+++ src/main/java/MyRunnable.java
@@ -1,6 +1,7 @@
 import org.apache.log4j.Logger;
 
 public class MyRunnable implements Runnable {
+    public static final int LIMIT = 100;
     private static final Logger logger = Logger.getLogger(MyRunnable.class);
     private final Counter counter;
 
@@ -10,7 +11,7 @@ public class MyRunnable implements Runnable {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < LIMIT; i++) {
             counter.incAndGet();
             logger.info(Thread.currentThread().getName() + " value = " + counter.get());
         }
--- src/main/java/MyThread.java
+++ src/main/java/MyThread.java
@@ -1,6 +1,7 @@
 import org.apache.log4j.Logger;
 
 public class MyThread extends Thread {
+    public static final int LIMIT = 100;
     private static final Logger logger = Logger.getLogger(MyThread.class);
     private final Counter counter;
 
@@ -10,7 +11,7 @@ public class MyThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < LIMIT; i++) {
             counter.incAndGet();
             logger.info(Thread.currentThread().getName() + " value = " + counter.get());
         }
